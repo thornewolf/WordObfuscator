a678c2e8b40891893711596a9bc6dd544242cc8f
Thorne Wolfenbarger <wolfenbarger@wolfenbarger-macbookpro.roam.corp.google.com>
2018-06-30 11:04:01 -0700

    Word Obfuscator commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 import random
+import string
+
+myString = "This is a test myString for testing purposes. The quick brown fox!! M!!!!!"
+ALPH = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def obfuscate(myString):
+	splitmyString = myString.split()
+	resultantSplit = []
+	for word in splitmyString:
+		if len([i for i in word if i not in string.punctuation]) <= 3:
+			resultantSplit.append(word)
+			continue
+		firstPart = word[0]
+		index = -1
+		while not any(char in ALPH for char in word[index:]):
+			index -= 1
+		lastPart = word[index:]
+		middlePart = ''.join(random.sample(word[1:index],len(word[1:index])))
+		resultantSplit.append(firstPart+middlePart+lastPart)
+	return ' '.join(resultantSplit)
+
+
+
+print(obfuscate(myString))
